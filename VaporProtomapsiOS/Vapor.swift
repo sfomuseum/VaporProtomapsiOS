1433402a52ebe5ee0a62efad00592faefc11fd65
thisisaaronland <thisisaaronland@localhost>
2025-12-08 15:02:27 -0800

    still wonky but prove that it works

--- VaporProtomapsiOS/Vapor.swift
+++ VaporProtomapsiOS/Vapor.swift
@@ -10,14 +10,17 @@ extension Application {
             allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
         )
         
+        // CORS stuff is necessary for PMTiles stuff
         let cors = CORSMiddleware(configuration: corsConfiguration)
         self.middleware.use(cors, at: .beginning)
+        
+        // I can not figure out how to make the default "Public" directory stuff work...
+        // https://docs.vapor.codes/advanced/middleware/
         
         guard let wwwBundlePath = Bundle.main.path(forResource: "www", ofType: "bundle") else {
             fatalError("Could not find www.bundle in app resources")
         }
         
-        // self.middleware.use(StripPrefixMiddleware(prefix: "/pmtiles/"))
         self.middleware.use(FileMiddleware(publicDirectory: wwwBundlePath))
         
         // The order here is important
--- VaporProtomapsiOS/www.bundle/javascript/index.js
+++ VaporProtomapsiOS/www.bundle/javascript/index.js
@@ -9,11 +9,14 @@ xhr.onreadystatechange = function() {
         map_el.innerHTML = "";
         const map = L.map('map');
             
-        const tile_url = "http://localhost:8080/pmtiles/sfo.pmtiles";
+        // const tile_url = "http://localhost:8080/pmtiles/sfo.pmtiles";
+        
+        const tile_url = "http://localhost:8080/ca.pmtiles";
         const tile_theme = "light";
         const tile_bounds = [ [37.601617, -122.408061], [37.640167, -122.354907] ];
         const tile_layer = protomapsL.leafletLayer({url: tile_url, theme: tile_theme});
-            
+        
+        
         console.log("TILES", tile_url);
         tile_layer.addTo(map);
         map.fitBounds(tile_bounds);
